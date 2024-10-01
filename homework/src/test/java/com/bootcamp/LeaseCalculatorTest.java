d7b6325b1e3f1cb6f5e4e335e7add2c51061651f
Agustin Arce <agustinarceprincich@gmail.com>
2024-10-01 15:34:02 -0400

    refactor (tests): split tests into individual scenarios

--- homework/src/test/java/com/bootcamp/LeaseCalculatorTest.java
+++ homework/src/test/java/com/bootcamp/LeaseCalculatorTest.java
@@ -6,50 +6,55 @@ import org.junit.jupiter.api.Test;
 
 public class LeaseCalculatorTest {
 
-    @Test
-    public void testCalculateMonthlyPayment() {
-        // Test case 1: Normal values
-        double totalLeasedMoney = 30000.0;
-        double downPayment = 5000.0;
-        int monthsLeaseTerm = 36;
-        double interestRate = 5.0;
+        @Test
+        public void testNormalValues() {
+                double totalLeasedMoney = 30000.0;
+                double downPayment = 5000.0;
+                int monthsLeaseTerm = 36;
+                double interestRate = 5.0;
 
-        double expectedMonthlyPayment = 729.17; // Corrected expected value
-        double actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment,
-                monthsLeaseTerm, interestRate);
-        assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+                double expectedMonthlyPayment = 729.17;
+                double actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment,
+                                monthsLeaseTerm, interestRate);
+                assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+        }
 
-        // Test case 2: Zero down payment
-        totalLeasedMoney = 30000.0;
-        downPayment = 0.0;
-        monthsLeaseTerm = 36;
-        interestRate = 5.0;
+        @Test
+        public void testZeroDownPayment() {
+                double totalLeasedMoney = 30000.0;
+                double downPayment = 0.0;
+                int monthsLeaseTerm = 36;
+                double interestRate = 5.0;
 
-        expectedMonthlyPayment = 875.00; // Calculated expected value
-        actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment, monthsLeaseTerm,
-                interestRate);
-        assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+                double expectedMonthlyPayment = 875.00;
+                double actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment,
+                                monthsLeaseTerm, interestRate);
+                assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+        }
 
-        // Test case 3: No interest
-        totalLeasedMoney = 30000.0;
-        downPayment = 5000.0;
-        monthsLeaseTerm = 36;
-        interestRate = 0.0;
+        @Test
+        public void testNoInterest() {
+                double totalLeasedMoney = 30000.0;
+                double downPayment = 5000.0;
+                int monthsLeaseTerm = 36;
+                double interestRate = 0.0;
 
-        expectedMonthlyPayment = 694.44; // Calculated expected value
-        actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment, monthsLeaseTerm,
-                interestRate);
-        assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+                double expectedMonthlyPayment = 694.44;
+                double actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment,
+                                monthsLeaseTerm, interestRate);
+                assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+        }
 
-        // Test case 4: High interest rate
-        totalLeasedMoney = 30000.0;
-        downPayment = 5000.0;
-        monthsLeaseTerm = 36;
-        interestRate = 20.0;
+        @Test
+        public void testHighInterestRate() {
+                double totalLeasedMoney = 30000.0;
+                double downPayment = 5000.0;
+                int monthsLeaseTerm = 36;
+                double interestRate = 20.0;
 
-        expectedMonthlyPayment = 833.3333333333334; // Calculated expected value
-        actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment, monthsLeaseTerm,
-                interestRate);
-        assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
-    }
+                double expectedMonthlyPayment = 833.33;
+                double actualMonthlyPayment = LeaseCalculator.calculateMonthlyPayment(totalLeasedMoney, downPayment,
+                                monthsLeaseTerm, interestRate);
+                assertEquals(expectedMonthlyPayment, actualMonthlyPayment, 0.01);
+        }
 }
